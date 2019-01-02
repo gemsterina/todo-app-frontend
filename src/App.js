c0df6bda815339dea3525d958a88b21d0822b527
Gemma <gemmalowe@gmail.com>
2019-01-02 16:49:13 +0000

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+This is going to be my to do list          </p>
           <a
             className="App-link"
             href="https://reactjs.org"
